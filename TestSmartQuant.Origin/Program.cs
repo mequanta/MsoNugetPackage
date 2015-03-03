02389a10fdf3d5cb488e0b8424daf09625567f9f
alexlee <lu.lee05@gmail.com>
2015-03-03 11:49:40 -0800

    add smartquant.orgin package

---
+++ TestSmartQuant.Origin/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SmartQuant;
+
+namespace TestSmartQuant.Origin
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var f = Framework.Current;
+			f.InstrumentManager.Dump ();
+			Console.WriteLine ();
+			f.DataManager.Dump ();
+			Console.WriteLine ("Done!");
+		}
+	}
+}
