e4ca0013696e82c66da7f2c4ee563490c8b99077
alex <alex@localhost.localdomain>
2015-03-06 20:07:10 +0800

    [MsoPlatform] remove *.Ide Dep

--- MsoPlatform/AddinInfo.cs
+++ MsoPlatform/AddinInfo.cs
@@ -8,4 +8,3 @@
 [assembly:AddinName ("MonoDevelop Mso Platform Support")]
 [assembly:AddinDescription ("Mso Platform Support for MonoDevelop")]
 [assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
-[assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
