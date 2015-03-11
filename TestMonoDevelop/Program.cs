c70490e0e82b0027683c91d5dde1ed80fcc714fe
alexlee <lu.lee05@gmail.com>
2015-03-11 21:25:27 +0800

    add testmdcore

--- TestMonoDevelop/Program.cs
+++ TestMonoDevelop/Program.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using MonoDevelop.Core;
 using MonoDevelop.Projects;
+using System.IO;
+using MonoDevelop.Core.ProgressMonitoring;
 
 namespace TestMonoDevelop
 {
@@ -12,7 +14,19 @@ namespace TestMonoDevelop
 			Runtime.Initialize (true);
 			foreach (var binding in LanguageBindingService.LanguageBindings)
 				LoggingService.LogInfo ("Loaded Language Binding: {0}", binding.Language);
-			Console.WriteLine("Done.");
+			var temp = Path.GetTempPath ();
+			var s = CreateSolution (temp, "mdcore");
+			Console.WriteLine ("Solution created at {0}", temp);
+		}
+
+		private static Solution CreateSolution (string dir, string name)
+		{
+			var solution = new Solution ();
+			var slnFile = Path.Combine (dir, string.Format ("{0}.sln", name));
+			solution.SetLocation (Path.GetDirectoryName (slnFile), Path.GetFileNameWithoutExtension (slnFile));
+			solution.CreateDefaultConfigurations ();
+			solution.Save (new NullProgressMonitor ());
+			return solution;
 		}
 	}
 }
