1c974a4f6ae77183e7f614b366f9d17afba737ca
alexlee <lu.lee05@gmail.com>
2015-04-10 18:59:21 +0800

    update to 5.8.3

---
+++ TestMdCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using MonoDevelop.Core;
+
+namespace TestMonoDevelop
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Runtime.Initialize (true);
+			Console.WriteLine ("Done");
+		}
+	}
+}
--- TestMonoDevelop/Program.cs
+++ TestMonoDevelop/Program.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using MonoDevelop.Core;
 using MonoDevelop.Projects;
 using System.IO;
-using MonoDevelop.Core.ProgressMonitoring;
 
 namespace TestMonoDevelop
 {
@@ -14,19 +13,6 @@ namespace TestMonoDevelop
 			Runtime.Initialize (true);
 			foreach (var binding in LanguageBindingService.LanguageBindings)
 				LoggingService.LogInfo ("Loaded Language Binding: {0}", binding.Language);
-			var temp = Path.GetTempPath ();
-			var s = CreateSolution (temp, "mdcore");
-			Console.WriteLine ("Solution created at {0}", temp);
-		}
-
-		private static Solution CreateSolution (string dir, string name)
-		{
-			var solution = new Solution ();
-			var slnFile = Path.Combine (dir, string.Format ("{0}.sln", name));
-			solution.SetLocation (Path.GetDirectoryName (slnFile), Path.GetFileNameWithoutExtension (slnFile));
-			solution.CreateDefaultConfigurations ();
-			solution.Save (new NullProgressMonitor ());
-			return solution;
 		}
 	}
 }
